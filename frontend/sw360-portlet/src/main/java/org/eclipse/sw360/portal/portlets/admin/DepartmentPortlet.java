10b05257f8af9d6734fcf56233e837ae3ece98ff
hienvv96 <hienv1.vuvanggmo@glb.toshiba.co.jp>
2022-02-18 10:24:10 +0700

    fix time

--- frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/portlets/admin/DepartmentPortlet.java
+++ frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/portlets/admin/DepartmentPortlet.java
@@ -48,7 +48,7 @@ import static org.eclipse.sw360.portal.common.PortalConstants.DEPARTMENT_PORTLET
 public class DepartmentPortlet extends Sw360Portlet {
     private static final Logger log = LogManager.getLogger(DepartmentPortlet.class);
     DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
-    Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"),Locale.getDefault());
+    Calendar calendar = Calendar.getInstance(Locale.getDefault());
     private static String temp;
     private static String lastRunningTime;
 
@@ -100,7 +100,6 @@ public class DepartmentPortlet extends Sw360Portlet {
     @UsedAsLiferayAction
     public void scheduleImportDepartment(ActionRequest request, ActionResponse response) throws PortletException {
         try {
-            dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+7"));
             temp = dateFormat.format(calendar.getTime());
             RequestSummary requestSummary =
                     new ThriftClients().makeScheduleClient().scheduleService(ThriftClients.IMPORT_DEPARTMENT_SERVICE);
@@ -148,7 +147,7 @@ public class DepartmentPortlet extends Sw360Portlet {
             request.setAttribute(WebKeys.REDIRECT, redirectUrl.toString());
             response.sendRedirect(redirectUrl.toString());
         } catch (IOException e) {
-            log.info("Error: {}", e.getMessage());
+            log.info("Error remove param url: {}", e.getMessage());
         }
     }
 
@@ -173,7 +172,6 @@ public class DepartmentPortlet extends Sw360Portlet {
     }
 
     private void importDepartmentManually(ResourceRequest request, ResourceResponse response) throws TException {
-        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+7"));
         temp = dateFormat.format(calendar.getTime());
         UserService.Iface userClient = thriftClients.makeUserClient();
         RequestSummary requestSummary = userClient.importFileToDB();
