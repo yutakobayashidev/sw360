7dbbccd9347c456b043fb974d5370c23f454aef9
tuan99123 <tuan2.nguyennhu@toshiba.co.jp>
2022-05-11 10:59:54 +0700

    feature(vulnerability):improve add/update/delete vulnerability APIs

--- frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/common/ErrorMessages.java
+++ frontend/sw360-portlet/src/main/java/org/eclipse/sw360/portal/common/ErrorMessages.java
@@ -63,7 +63,12 @@ public class ErrorMessages {
     public static final String LICENSE_TYPE_ACCESS_DENIED = "User does not have the permission to add license type.";
     public static final String OBLIGATION_NOT_ADDED = "Obligation could not be added.";
     public static final String OBLIGATION_NOT_UPDATED = "Obligation could not be updated.";
+    public static final String ERROR_VULNERABILITY_USED_BY_RELEASE = "Can not remove vulnerability because it is used by releases";
+    public static final String ERROR_VULNERABILITY_DELETE = "Error when delete vulnerability";
 
+    public static final String ERROR_ADD_VULNERABILITY = "Error when add vulnerability";
+
+    public static final String ERROR_UPDATE_VULNERABILITY = "Error when edit vulnerability";
     //this map is used in errorKeyToMessage.jspf to generate key-value pairs for the liferay-ui error tag
     public static final ImmutableList<String> allErrorMessages = ImmutableList.<String>builder()
             .add(PROJECT_NOT_ADDED)
@@ -113,6 +118,10 @@ public class ErrorMessages {
             .add(ERROR_USER_ACTIVATE_DEACTIVATE)
             .add(OBLIGATION_NOT_ADDED)
             .add(OBLIGATION_NOT_UPDATED)
+            .add(ERROR_VULNERABILITY_USED_BY_RELEASE)
+            .add(ERROR_VULNERABILITY_DELETE)
+            .add(ERROR_ADD_VULNERABILITY)
+            .add(ERROR_UPDATE_VULNERABILITY)
             .build();
 
     private ErrorMessages() {
